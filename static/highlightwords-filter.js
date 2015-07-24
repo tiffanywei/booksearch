bc8c0c80d1e6020d8f9ebc8067671cacae22001b
Tiffany Wei <tiffanywei90@gmail.com>
2015-07-24 12:51:34 -0700

    fix formatting highlight words

--- static/highlightwords-filter.js
+++ static/highlightwords-filter.js
@@ -1,5 +1,11 @@
 (function() {
 
+	/**
+	/* Formats the results object so that the sentences for each word has the
+	/* html markup for highlighting.
+	/* @param results object
+	/* @returns results object
+	*/
 	angular.module('bookSearch')
 			.filter('highlightWords', ['$sce', function($sce) {
 				return function(results) {
@@ -9,28 +15,8 @@
 								new RegExp('('+word+')', 'gi'), '<span class="highlighted">$1</span>'));
 						});
 					});
-					console.log(results);
 					return results;
 				}
 			}]);
-
-	/**
-	/* Formats the results object so that the sentences for each word has the
-	/* html markup for highlighting.
-	/* @param results object
-	/* @returns results object
-	*/
-	function highlightWords(results) {
-		var http = $http;
-		var sce = $sce;
-		angular.forEach(results, function(wordInfoList, word) {
-			angular.forEach(wordInfoList, function(wordInfo) {
-				wordInfo.sentence = sce.trustAsHtml(wordInfo.sentence.replace(
-					new RegExp('('+word+')', 'gi'), '<span class="highlighted">$1</span>'));
-			});
-		});
-		console.log(results);
-		return results;
-	}
 
 })();
